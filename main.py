1436a7783e552805f39c2670606de28f0965f091
Noga Dayan <110382@st-maartenscollege.nl>
2020-07-03 20:58:32 +0000

    eerste message, de strepen voor de letters en de tekening op display gezet.

--- main.py
+++ main.py
@@ -12,6 +12,10 @@ def play(word):
     guessed_letters = []
     guessed_words = []
     tries = 6
+    print("Let's play Hangman!")
+    print(display_hangman(tries))
+    print(word_completion)
+    print("\n")
 
 
 
